e4c81a63415d2be7b6fd908e2569000af18aab58
Tiago Pereira <tiago@MacBook-Pro.local>
2021-03-23 13:31:46 +0100

    removed env

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ app.use("/api/posts", posts);
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 
 // Server static assets if in production
-if (process.env.NODE.ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE.ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 5000; // First port for deployment, second for local development
 
